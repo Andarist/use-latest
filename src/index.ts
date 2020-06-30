254e327210ae053065a64e97ce0d5e294a21eae9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-06-30 15:51:42 +0200

    Update ref using use-isomorphic-layout-effect

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import useIsomorphicLayoutEffect from 'use-isomorphic-layout-effect'
 
 const useLatest = <T>(value: T) => {
   const ref = React.useRef(value)
 
-  React.useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     ref.current = value
   })
 
